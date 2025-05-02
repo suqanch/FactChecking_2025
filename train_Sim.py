e5ac68da3304dacda1ce9902c5674ff0959849ca
Squanch <90013237+suqanch@users.noreply.github.com>
2025-05-02 15:05:31 +1000

    update now can run on colab

--- train_Sim.py
+++ train_Sim.py
@@ -15,7 +15,8 @@ import random
 import pandas as pd
 import time
 from tqdm import tqdm
-    
+from torch.cuda.amp import autocast, GradScaler
+ 
 
 def load_train_data_supervised(tokenizer, file_path, max_length = 512):
     feature_list = []
@@ -30,7 +31,7 @@ def load_train_data_supervised(tokenizer, file_path, max_length = 512):
             [sent0, sent1, hard_neg],
             max_length=max_length,
             truncation=True,
-            padding='longest',
+            padding='max_length',
             return_tensors='pt'
         )
         # feature 是个 dict，包含：
@@ -56,7 +57,7 @@ def train_sup(model, train_loader, optimizer, device, epochs = 5, train_mode = '
             input_ids = data['input_ids'].view(-1, sql_len).to(device)
             attention_mask = data['attention_mask'].view(-1, sql_len).to(device)
             token_type_ids = data['token_type_ids'].view(-1, sql_len).to(device)
-
+            # logger.info('debug')
             out = model(input_ids, attention_mask, token_type_ids)
             if train_mode == 'unsupervise':
                 loss = simcse_unsup_loss(out, device)
@@ -65,7 +66,7 @@ def train_sup(model, train_loader, optimizer, device, epochs = 5, train_mode = '
         
             loss.backward()
             step += 1
-            
+
             if (step + 1) % accumulation_steps == 0:
                 optimizer.zero_grad()
                 optimizer.step()
@@ -73,18 +74,55 @@ def train_sup(model, train_loader, optimizer, device, epochs = 5, train_mode = '
             if step % 100 == 0:
                 logger.info(f"epoch: {epoch}, step: {step}, loss: {loss.item()}")
 
-    return model
+    return 0
+
+# def AMP_train_sup(model, train_loader, optimizer, device, epochs = 5, train_mode = 'supervise'):
+#     logger.info("AMP start training")
+#     model.train()
+#     accumulation_steps = 4
+#     scaler = GradScaler()
+#     for epoch in range(epochs):
+#         for batch_idx, data in enumerate(tqdm(train_loader)):
+#             step = epoch * len(train_loader) + batch_idx
+#             # [batch, n, seq_len] -> [batch * n, sql_len]
+#             sql_len = data['input_ids'].shape[-1]
+#             input_ids = data['input_ids'].view(-1, sql_len).to(device)
+#             attention_mask = data['attention_mask'].view(-1, sql_len).to(device)
+#             token_type_ids = data['token_type_ids'].view(-1, sql_len).to(device)
+
+#             with autocast():
+#                 out = model(input_ids, attention_mask, token_type_ids)
+#                 if train_mode == 'unsupervise':
+#                     loss = simcse_unsup_loss(out, device)
+#                 else:
+#                     loss = simcse_sup_loss(out, device)
+
+#             scaler.scale(loss).backward()
+#             step += 1
+
+#             if (step + 1) % accumulation_steps == 0:
+#                 optimizer.zero_grad()
+#                 scaler.step(optimizer)
+#                 scaler.update()            
+#             if step % 100 == 0:
+#                 logger.info(f"epoch: {epoch}, step: {step}, loss: {loss.item()}")
+
+#     return 0
 
 if __name__ == '__main__':
-    pass
-    # batch_size = 64
-    # file_path = 'data/train-embed.csv'
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # checkpoint = "bert-base-uncased"
-    # tokenizer = BertTokenizer.from_pretrained(checkpoint)
-    # model = SimcseModel(pretrained_model=checkpoint, pooling='pooler', dropout=0.1).to(device)
-    # train_data = load_train_data_supervised(tokenizer, file_path)
-    # train_dataset = TrainDataset(train_data, tokenizer)
-    # train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
-    # optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-5)
-    # train_sup(model, train_dataloader, optimizer, device, epochs = 5, train_mode = 'supervise')
+    batch_size = 64
+    file_path = 'data/train-embed.csv'
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    print(f"Using device: {device}")
+    checkpoint = "bert-base-uncased"
+    tokenizer = BertTokenizer.from_pretrained(checkpoint)
+    model = SimcseModel(pretrained_model=checkpoint, pooling='pooler', dropout=0.1).to(device)
+    train_data = load_train_data_supervised(tokenizer, file_path)
+    train_dataset = TrainDataset(train_data, tokenizer)
+    train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+    optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-5)
+    train_sup(model, train_dataloader, optimizer, device, epochs = 5, train_mode = 'supervise')
